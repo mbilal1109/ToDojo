8abe4bf01f20302c8e3535f242a473bbad2a2945
Mohammad Yousuf <mohammadbil1998@gmail.com>
2024-09-30 21:44:31 -0500

    Adding HTML Structure & Styling

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import "./styles.css";
 
-console.log("Hello");
+let addButton = document.querySelector("#add-btn");
+let dialog = document.querySelector("#dialog");
+
+addButton.addEventListener("click", () => {
+  dialog.showModal();
+});
